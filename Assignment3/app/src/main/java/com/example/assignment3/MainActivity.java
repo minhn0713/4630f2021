2616f51f6febbe89bb0f02ff4855bbc622dfd464
Minh Nguyen <minhnguyen130798@gmail.com>
2021-10-07 01:15:49 -0400

    added view my resume

--- Assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
+++ Assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.assignment3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +12,14 @@ public class MainActivity extends AppCompatActivity {
     private Button invest_port_button;
     private Button facts_button;
     private Button self_intro_button;
+    private Button resume_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView text = (TextView) findViewById(R.id.resume_textview);
+        //text.setMovementMethod(LinkMovementMethod.getInstance());
 
         // self intro button
         self_intro_button = (Button) findViewById(R.id.self_intro_button);
@@ -43,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
                 openInvestPort();
             }
         });
+
+        resume_button = (Button) findViewById(R.id.resume_button);
+        resume_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResume();
+            }
+        });
     }
 
     // open self intro activity
@@ -60,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
     // open investment portfolio activity
     public void openInvestPort() {
         Intent intent = new Intent(this, InvestPortActivity.class);
+        startActivity(intent);
+    }
+
+    // open my resume link
+    public void openResume() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/document/d/1DIijGViD4-7gA5FWW1K7p4QNjjsp1lDx/edit?usp=sharing&amp;ouid=109571095316197278402&amp;rtpof=true&amp;sd=true"));
         startActivity(intent);
     }
 
