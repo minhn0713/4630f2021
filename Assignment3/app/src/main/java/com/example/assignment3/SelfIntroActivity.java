5d9487111c2df55228f210b67dabb6fb879e9872
Minh Nguyen <minhnguyen130798@gmail.com>
2021-10-06 23:01:30 -0400

    Added custom bg, fonts, info for self intro activity

--- Assignment3/app/src/main/java/com/example/assignment3/SelfIntroActivity.java
+++ Assignment3/app/src/main/java/com/example/assignment3/SelfIntroActivity.java
@@ -3,11 +3,15 @@ package com.example.assignment3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SelfIntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selfinto);
     }
+
 }
