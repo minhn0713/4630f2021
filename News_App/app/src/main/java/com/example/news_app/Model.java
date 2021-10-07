58953cddb664c7068bf6678680e599d637913993
Minh Nguyen <minhnguyen130798@gmail.com>
2021-10-07 19:12:13 -0400

    initiated project and design layout

---
+++ News_App/app/src/main/java/com/example/news_app/Model.java
@@ -0,0 +1,9 @@
+package com.example.news_app;
+
+import java.util.ArrayList;
+
+public class Model {
+    private int totalResults;
+    private String status;
+    private ArrayList<WallStreetHeadlines> wallstreetheadlines;
+}
