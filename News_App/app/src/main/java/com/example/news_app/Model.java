fe973f336770e1e3b464503b498c282063cb876d
Minh Nguyen <minhnguyen130798@gmail.com>
2021-11-03 16:34:27 -0400

    Delete News_App directory

--- News_App/app/src/main/java/com/example/news_app/Model.java
+++
@@ -1,9 +0,0 @@
-package com.example.news_app;
-
-import java.util.ArrayList;
-
-public class Model {
-    private int totalResults;
-    private String status;
-    private ArrayList<WallStreetHeadlines> wallstreetheadlines;
-}
