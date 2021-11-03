9f9b064f0f64e1805b934de74cfb9c486750d021
Minh Nguyen <minhnguyen130798@gmail.com>
2021-11-03 16:56:56 -0400

    create layout & assign variables

---
+++ New_World/app/src/main/java/com/example/new_world/Model.java
@@ -0,0 +1,10 @@
+
+package com.example.new_world;
+
+import java.util.ArrayList;
+
+public class Model {
+    private int totalResults;
+    private String status;
+    private ArrayList<WallStreetHeadlines> wallstreetheadlines;
+}
